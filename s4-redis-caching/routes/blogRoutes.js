662dc88c65c41741444f2e1973d79c2e983a562d
CICADA3310 <36794918+git-chinmay@users.noreply.github.com>
2023-05-16 09:04:12 +0530

    redis caching

--- s4-redis-caching/routes/blogRoutes.js
+++ s4-redis-caching/routes/blogRoutes.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
+const { cleanCache } = require('../middlewares/cleanCache');
 
 const Blog = mongoose.model('Blog');
 
@@ -14,29 +15,10 @@ module.exports = app => {
   });
 
   app.get('/api/blogs', requireLogin, async (req, res) => {
-    const redis = require('redis');
-    const redisURL = 'redis://127.0.0.1:6379'
-    const client = redis.createClient(redisURL);
 
-    //converting redis into a promise as bydefaut its not
-    //and we dont want to use callback while 'get' data from cache
-    const util = require('util');
-    client.get = util.promisify(client.get);
-
-    /*
-    We want first check the query in redis before hitting db.
-    If req present then return data right away otherwise get the data from db
-    and store it in Redis and retun to user
-    */
-
-    //userid is our key
-    //NO need to use callback as we are returning a promise due to promisify
-    const cachedBlog = await client.get(req.user.id);
-
-
-    const blogs = await Blog.find({ _user: req.user.id });
-
+    const blogs = await Blog.find({ _user: req.user.id }).cache({key:req.user.id});
     res.send(blogs);
+
   });
 
   app.post('/api/blogs', requireLogin, async (req, res) => {
@@ -54,5 +36,9 @@ module.exports = app => {
     } catch (err) {
       res.send(400, err);
     }
+
+    // Clear nested hash after blog created and saved
+    //clearHash(req.user.id);
+    //We will not using this approach instead we will use middleware to automate it more
   });
 };
