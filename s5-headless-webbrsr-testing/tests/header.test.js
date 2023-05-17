0c23f3edbddb6a07169dde94ecede7f725865617
CICADA3310 <36794918+git-chinmay@users.noreply.github.com>
2023-05-17 10:10:04 +0530

    starting chromium test

--- s5-headless-webbrsr-testing/tests/header.test.js
+++ s5-headless-webbrsr-testing/tests/header.test.js
@@ -1,4 +1,19 @@
+const puppeteer = require('puppeteer');
+
 test('Adds two numbers', ()=>{
     const sum = 1 + 2;
     expect(sum).toEqual(3);
+});
+
+test('Launching a chromium browser', async ()=>{
+    const browser = await puppeteer.launch({
+        headless: false
+    });
+    const page = await browser.newPage();
+    // Visiting the blogpost page
+    await page.goto('localhost:3000');
+
+    //Extacting the blog title
+    const text = await page.$eval('a.brand-logo', el => el.innerHTML);
+    expect(text).toEqual('Blogster');
 });
