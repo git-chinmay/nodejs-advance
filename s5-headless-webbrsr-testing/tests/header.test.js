dcb125ac165b9cf0c28014d87a56dcf53d2577b0
CICADA3310 <36794918+git-chinmay@users.noreply.github.com>
2023-05-18 10:05:17 +0530

    Testing oAuth flow

--- s5-headless-webbrsr-testing/tests/header.test.js
+++ s5-headless-webbrsr-testing/tests/header.test.js
@@ -1,19 +1,40 @@
 const puppeteer = require('puppeteer');
 
-test('Adds two numbers', ()=>{
-    const sum = 1 + 2;
-    expect(sum).toEqual(3);
-});
+// test('Adds two numbers', ()=>{
+//     const sum = 1 + 2;
+//     expect(sum).toEqual(3);
+// });
 
-test('Launching a chromium browser', async ()=>{
-    const browser = await puppeteer.launch({
+
+var browser, page;
+//Will run before each test
+beforeEach(async ()=>{
+    browser = await puppeteer.launch({
         headless: false
     });
-    const page = await browser.newPage();
+    page = await browser.newPage();
     // Visiting the blogpost page
     await page.goto('localhost:3000');
+})
 
+//Will run after each test
+afterEach(async ()=>{
+    await browser.close();
+});
+
+
+test('The header has the correct text.', async ()=>{
     //Extacting the blog title
     const text = await page.$eval('a.brand-logo', el => el.innerHTML);
     expect(text).toEqual('Blogster');
 });
+
+
+
+test('Clicking login starts OAuth flow', async ()=>{
+    await page.click('.right a');
+    const url = await page.url();
+    expect(url).toMatch('/accounts\.google\.com/'); //\ > Escape character
+});
+
+
