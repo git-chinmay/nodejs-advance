b8ed874833a8a4ad4ca5184f369bd17a8d16a7d2
CICADA3310 <36794918+git-chinmay@users.noreply.github.com>
2023-06-10 11:25:29 +0530

    get signed url

--- s7-scalable-image-file-upload/routes/uploadRoutes.js
+++ s7-scalable-image-file-upload/routes/uploadRoutes.js
@@ -1,5 +1,7 @@
 const AWS = require('aws-sdk');
 const keys = require('../config/keys');
+const uuid = require('uuid/v1'); //v1 version1. u cannuse any version
+const requireLogin = require('../middlewares/requireLogin');
 
 const s3 = new AWS.S3({
     credentials: {
@@ -11,7 +13,15 @@ const s3 = new AWS.S3({
 
 
 module.exports = app => {
-    app.get('/api/upload', (req, res)=>{
+    app.get('/api/upload', requireLogin, (req, res)=>{
+      const key = `${req.user.id}/${uuid()}.jpeg`;
 
+      s3.getSignedUrl('putObject', {
+        Bucket: 'myblogster-bucket',
+        ContentType: 'jpeg',
+        Key: key
+      }, (err, url)=>{
+          res.send({key, url})
+      })
     })
 };
